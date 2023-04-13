6e0537295351d7e120f5ee69c00b3a81a6387d59
signorrayan <signor.rayan@gmail.com>
2023-04-13 18:56:10 +0200

    Integrate with Terraform Cloud

--- backend.tf
+++ backend.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "signorrayan"
+    workspaces {
+      name = "aws-elb-asg-terraform-launch-template"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- variables.tf
+++ variables.tf
@@ -224,3 +224,13 @@ variable "sns_topic_protocol" {
   type    = string
   default = "email"
 }
+
+################################################
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
